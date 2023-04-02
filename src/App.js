6e67352da8865bffec68189ee92c53c51e3dd2c3
Mahesh Abhale <abhale.mahesh@iitgn.ac.in>
2023-04-02 23:30:51 +0530

    react-router implementation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
 
@@ -38,12 +44,16 @@ function App() {
   return (
     <>
     {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     <Alert alert={alert}/>
     <div className="container my-3">
-      <TextForm showAlert={showAlert} heading="Enter the text to Analyse" mode={mode}/>
-      <About/>
+      <Routes>
+          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to Analyse" mode={mode}/>} />
+      </Routes>
     </div>
+    </Router>
     </>
   );
 }
